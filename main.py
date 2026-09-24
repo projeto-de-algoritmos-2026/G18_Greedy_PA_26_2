666c15f0592298748acd063d1c4d1bbfbe80f9a6
Josef Wojtyla <josefwojtylabarrosdesouza@gmail.com>
2026-09-24 17:39:03 -0300

    fazendo menu

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #--- import sistema
 import curses
+from minigame import testbench
 from minigame import rodada
 
 #--- variaveis do jogo
@@ -9,3 +10,24 @@ game_var = {
     'tempo_limite' : 20
 }
 
+def menu_principal(stdscr):
+    curses.curs_set(0)
+
+    while True:
+        stdscr.erase()
+        stdscr.border()
+
+        stdscr.addstr(2, 4, "=== TROCO AMBICIOSO ===", curses.A_BOLD)
+        stdscr.addstr(4, 4, "1 - Iniciar Expediente (Jogar)")
+        stdscr.addstr(5, 4, "2 - Fechar a Loja (Sair)")
+        stdscr.addstr(7, 4, "Escolha uma opcao: ")
+        stdscr.refresh()
+
+        if stdscr.getch() == 1:
+            testbench(stdscr)
+        elif stdscr.getch() == 2:
+            break
+
+
+if __name__ == "__main__":
+    curses.wrapper(menu_principal)
