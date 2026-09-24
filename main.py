f813ce87e9ab43f09e478440b876f6fdf0c7cc3f
Eduardo S. Brito - archlinux laptop <edusoubrit09@gmail.com>
2026-09-24 15:39:21 -0300

    Cria arquivo main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+#--- import sistema
+import curses
+from minigame import rodada
+
+#--- variaveis do jogo
+game_var = {
+    'moedas' : [500,100,50,10,5,1],
+    'pontuacao_por_rodada' : 100,
+    'tempo_limite' : 20
+}
+
